a5de84a0f15f879564299980e702aad4282b74b6
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 20:11:58 +0000

    Alert.js: changed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,9 @@
-const Alert = ({ body }) => {
-    return <p>{body}</p>
+const Alert = ({ annotationPrefix, body }) => {
+    return (
+        <div data-sb-field-path={annotationPrefix}>
+            <p data-sb-field-path=".body">{body}</p>
+        </div>
+    )
 }
 
 export default Alert
