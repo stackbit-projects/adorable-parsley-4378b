f4f1a6fa7b59e99078825ae4831d3a2b6146391f
Stackbit Code Editor <projects@stackbit.com>
2021-11-04 20:21:07 +0000

    Alert.js: changed

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,7 @@
 const Alert = ({ annotationPrefix, body }) => {
     return <div data-sb-field-path={annotationPrefix} className="py-6 bg-primary">
         <div className="container mx-auto">
-        <p data-sb-field-path=".body" className="">{body}</p>
+            <p data-sb-field-path=".body" className="text-center text-xl text-white">{body}</p>
         </div>
     </div>
 }
